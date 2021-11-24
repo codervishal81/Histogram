b2e9614d121e2767710d11d9d656f0e2bb7e6065
vishal shah <vishal@imorphr.com>
2021-11-24 20:30:25 +0530

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -1,3 +1,4 @@
+'''Word Histogram for file'''
 import string
 
 class HistogramData:
@@ -7,6 +8,7 @@ class HistogramData:
         self.word_frequency = dict()
 
     def process_line(self, line):
+        '''Prepares word histogram for a line'''
         line = line.replace('-', ' ')
         for word in line.split():
             word = word.strip(string.punctuation + string.whitespace)
@@ -14,11 +16,13 @@ class HistogramData:
             self.word_frequency[word] = self.word_frequency.get(word, 0) + 1
 
     def process_file(self, filename):
-        fp = open(filename, encoding="utf8")
-        for line in fp:
+        '''Prepares word histogram for a file'''
+        file_to_process = open(filename, encoding="utf8")
+        for line in file_to_process:
             self.process_line(line)
 
     def sort_hist_by_freq_desc(self, limit=10):
+        '''Sorts histogram'''
         sorted_list = []
         for key, val in self.word_frequency.items():
             sorted_list.append((val, key))
@@ -27,7 +31,9 @@ class HistogramData:
         return sorted_list[0:limit] if limit < len(sorted_list) else sorted_list
 
     def total_words(self):
+        '''Counts total words'''
         return sum(self.word_frequency.values())
 
     def different_words(self):
+        '''Counts different words'''
         return len(self.word_frequency)
